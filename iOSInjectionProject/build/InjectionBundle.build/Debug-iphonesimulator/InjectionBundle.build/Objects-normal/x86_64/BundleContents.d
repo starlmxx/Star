d0e9f6034623e891359135bf1238d6e35651a7a2
limingxing <star@limingxingdemacbook-pro.local>
2016-04-16 20:25:51 +0800

    initial project

--- iOSInjectionProject/build/InjectionBundle.build/Debug-iphonesimulator/InjectionBundle.build/Objects-normal/x86_64/BundleContents.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/star/ios/develop/Star/iOSInjectionProject/BundleContents.m \
-  /tmp/injectionforxcode/BundleInjection.h \
-  /tmp/injectionforxcode/BundleInterface.h \
-  /Users/star/ios/develop/Star/iOSInjectionProject/BundleContents.h
